feac5d26a5b49a4a8fd681ce2bcf5adc4859f948
PaulEzennolim <Paulezennolim8@gmail.com>
2024-10-22 22:08:38 +0100

    password_vault.py grids and tables added

--- password_vault.py
+++ password_vault.py
@@ -1,5 +1,7 @@
 import sqlite3, hashlib # sqlite3 is for the database, hashlib is to hash the password
 from tkinter import * # tkinter is for the window
+from tkinter import simpledialog # Pop ups to ask for the password, username etc
+from functools import partial
 
 # Database code
 with sqlite3.connect("password_vault.db") as db:
@@ -10,6 +12,19 @@ CREATE TABLE IF NOT EXISTS masterpassword(
 id INTEGER PRIMARY KEY,
 password TEXT NOT NULL);
 """)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vault(
+id INTEGER PRIMARY KEY,
+website TEXT NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL);
+""")
+
+# Create popup
+def popup(text):
+    answer = simpledialog.askstring("Input string", text) # Creates a popup and asks for a string
+    return answer # When def popup is called program will get what the user enetered
 
 window = Tk() # Initiate window
 window.title("Password Vault")
@@ -104,11 +119,76 @@ def passwordVault(): # When the user enters the right password, they are brought
     '''
     for widget in window.winfo_children():
         widget.destroy()
-    window.geometry("700x350")
 
+    def addEntry():
+        # The text the program will give the popup function
+        text1 = "Website"
+        text2 = "Username"
+        text3 = "Password"
+        website = popup(text1)
+        username = popup(text2)
+        password = popup(text3)
+
+        insert_fields = """INSERT INTO vault(website, username, password)
+        Values(?, ?, ?)"""
+        cursor.execute(insert_fields, (website, username, password))
+        db.commit()
+        passwordVault() # Refreshes the screen
+    
+    def removeEntry(input):
+        # The difference between all the entries is the id, so the program will use the id as an identifier 
+        cursor.execute("DELETE FROM vault WHERE id = ?", (input,))
+        db.commit()
+        passwordVault()
+
+    window.geometry("700x350")
+    
     lbl = Label(window, text="Password Vault")
-    lbl.config(anchor=CENTER) # config allows everything to be in the middle
-    lbl.pack()
+    lbl.grid(column=1)
+
+    btn = Button(window, text="+", command=addEntry) # Button to add a password, username etc
+    btn.grid(column=1, pady=10)
+
+    lbl = Label(window, text="Website")
+    lbl.grid(row=2, column=0, padx=80)
+    lbl = Label(window, text="Username")
+    lbl.grid(row=2, column=1, padx=80)
+    lbl = Label(window, text="Password")
+    lbl.grid(row=2, column=2, padx=80)
+
+    cursor.execute("SELECT * FROM vault")
+    
+    if (cursor.fetchall != None): # Makes sure that their is something in the vault db
+        i = 0
+        while True:
+            cursor.execute("SELECT * FROM vault")
+            """
+            The db is displayed as an array. Each item is going to have the website, username and password. Second
+            item will have a different website, username and password etc. So the program is going to get each item
+            and display it.
+            """
+            array = cursor.fetchall()
+            lbl = Label(window, text=(array[i][1]), font=("Helvetica", 12)) # website 
+            lbl.grid(column=0, row=i+3)
+            lbl = Label(window, text=(array[i][2]), font=("Helvetica", 12)) # Username
+            lbl.grid(column=1, row=i+3)
+            lbl = Label(window, text=(array[i][3]), font=("Helvetica", 12)) # Password
+            lbl.grid(column=2, row=i+3)
+
+            btn = Button(window, text="Delete", command=partial(removeEntry, array[i][0]))
+            btn.grid(column=3, row=i+3, pady=10)
+
+            i = i + 1 # Every time the while loop runs, i will increase
+            cursor.execute("SELECT * FROM vault") # Stops the loop
+            
+            if (len(cursor.fetchall())) <= i: # If the length of the array is less than or equal to i
+                """
+                Imagine the length of the array is 5, so the program has 5 different entries. When the program runs, 
+                it'll go to 1, then 2, then up to 5. At 5 the program will get the fifth entry and then the program will 
+                stop running because there is no other entry. 
+                """
+                break
+
 
 cursor.execute("SELECT * FROM masterpassword")
 
